b2a9f31cf4459d2737f4ca3368827db23e370f42
Misael Guerrero <mguerrerowest@gmail.com>
2023-03-23 09:04:15 -0400

    create 3 event listeners to handle tab switing

--- src/index.js
+++ src/index.js
@@ -4,5 +4,42 @@ import contact from "./contact";
 import './style.css' 
 
 let content = document.querySelector('#content')
+content.appendChild(home)
 
-content.appendChild(contact);
+const tabs = (() =>{
+    function displayCarta(){
+        document.addEventListener('click', e =>{
+            if (e.target.matches('.menu-btn')){
+                content.innerHTML = ''
+                content.appendChild(carta)
+            } else if (e.target.matches ('.tab-menu')){
+                content.innerHTML = ''
+                content.appendChild(carta)
+            }
+        })
+    }
+    
+    function displayHome(){
+        document.addEventListener('click', e =>{
+            if (e.target.matches('.tab-home')){
+                content.innerHTML = ''
+                content.appendChild(home)
+            }
+        })
+    }
+    
+    function displayContact(){
+        document.addEventListener('click', e =>{
+            if (e.target.matches('.tab-contact')){
+                content.innerHTML = ''
+                content.appendChild(contact)
+            }
+        })
+    }
+
+    return { displayHome, displayCarta, displayContact }
+})();
+
+tabs.displayHome();
+tabs.displayCarta();
+tabs.displayContact();
