53c08959eeabaab77b9a518f8371dbadbc121023
Misael Guerrero <mguerrerowest@gmail.com>
2023-03-22 22:22:46 -0400

    create stylesheet and install css loaders

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import home from "./home";
 import carta from "./carta";
 import contact from "./contact";
+import './style.css' 
 
 let content = document.querySelector('#content')
 
-content.appendChild(home);
+content.appendChild(contact);
